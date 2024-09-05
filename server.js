2b228ab5a2fef96f93cdd59a8e32ab312a85296c
nathan <nathanlogan81@gmail.com>
2024-09-05 15:39:15 -0500

    feat: bcrypt

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt      = require('bcrypt');
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
@@ -11,13 +12,23 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res);
+    })
+});
+
 
 
 //END_ASYNC
 
 //START_SYNC
 
-
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
